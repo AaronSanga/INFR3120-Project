557473df4e78c0b1fb9fe5a0ecd3052e940b5543
AaronSanga <aaron.sanga@ontariotechu.net>
2024-11-25 00:25:43 -0500

    Fixed issue with splash page not defining "TaskList"

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,105 +4,121 @@ const passport = require('passport');
 const Db = require('../config/db');
 let userModel = require('../model/User');
 let User = userModel.User;
+let Task = require('../model/task');
 
 /* GET index page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home', 
-    displayName: req.user?req.user.displayName:''
-  });
-});
-/* GET home page. */
-router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home', 
-    displayName: req.user?req.user.displayName:''
-  });
+router.get('/', async function(req, res, next) {
+  try {
+    const TaskList = await Task.find();
+    res.render('index', { 
+      title: 'Home', 
+      displayName: req.user ? req.user.displayName : '',
+      TaskList: TaskList
+    });
+  } catch (err) {
+    console.error(err);
+    res.render('index', { 
+      title: 'Home',
+      displayName: req.user ? req.user.displayName : '',
+      TaskList: []
+    });
+  }
 });
 
-router.get('/login', function(req,res,next){
-  if(!req.user)
-  {
-    res.render('Auth/login' ,
-      {
-      title:'Login',
-      message:req.flash('loginMessage'),
-      displayName: req.user ?  req.user.displayName:''
-      }
-  )
+/* GET home page. */
+router.get('/home', async function(req, res, next) {
+  try {
+    // Fetch the TaskList from the database
+    const TaskList = await Task.find();
+    res.render('index', { 
+      title: 'Home', 
+      displayName: req.user ? req.user.displayName : '',
+      TaskList: TaskList
+    });
+  } catch (err) {
+    console.error(err);
+    res.render('index', { 
+      title: 'Home',
+      displayName: req.user ? req.user.displayName : '',
+      TaskList: []
+    });
   }
-  else{
-    return res.redirect('/')
+});
+
+router.get('/login', function(req, res, next) {
+  if (!req.user) {
+    res.render('Auth/login', {
+      title: 'Login',
+      message: req.flash('loginMessage'),
+      displayName: req.user ? req.user.displayName : ''
+    });
+  } else {
+    return res.redirect('/');
   }
-})
-router.post('/login', function(req,res,next){
-  passport.authenticate('local' ,(err,user,info)=>{
-    if (err)
-    {
-      return next(err)
+});
+
+router.post('/login', function(req, res, next) {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return next(err);
     }
-    if(!user)
-    {
+    if (!user) {
       req.flash('loginMessage', 'Authentication Error');
       return res.redirect('/login');
     }
-    req.login(user,(err)=>{
-      if (err)
-      {
-        return next(err)
+    req.login(user, (err) => {
+      if (err) {
+        return next(err);
       }
-      return res.redirect('/tasklist')
-    })
-  })(req, res, next)
-})
-router.get('/register', function(req,res,next){
-  if(!req.user)
-  {
-    res.render('Auth/register',{
-      title:'Register',
-      message:req.flash('registerMessage'),
-      displayName: req.user?req.user.displayName:''
-    })
+      return res.redirect('/tasklist');
+    });
+  })(req, res, next);
+});
+
+router.get('/register', function(req, res, next) {
+  if (!req.user) {
+    res.render('Auth/register', {
+      title: 'Register',
+      message: req.flash('registerMessage'),
+      displayName: req.user ? req.user.displayName : ''
+    });
+  } else {
+    return res.redirect('/');
   }
-  else
-  {
-    return res.redirect('/')
-  }
-})
-router.post('/register',function(req,res,next){
+});
+
+router.post('/register', function(req, res, next) {
   let newUser = new User({
-    username:req.body.username,
-    //password:req.body.password,
-    email:req.body.email,
-    displayName:req.body.displayName
-  })
-  User.register(newUser,req.body.password,(err)=>{
-    if(err)
-    {
+    username: req.body.username,
+    email: req.body.email,
+    displayName: req.body.displayName
+  });
+  User.register(newUser, req.body.password, (err) => {
+    if (err) {
       console.log("Error: Inserting the new User");
-      if(err.name=="UserExistError")
-      {
-        req.flash('registerMessage','Registration Error: User already exists')
+      if (err.name == "UserExistError") {
+        req.flash('registerMessage', 'Registration Error: User already exists');
       }
-      return res.render('Auth/register',{
-        title:'Register',
-        message:req.flash('registerMessage'),
-        displayName:req.user?req.user.displayName:''
-      })
+      return res.render('Auth/register', {
+        title: 'Register',
+        message: req.flash('registerMessage'),
+        displayName: req.user ? req.user.displayName : ''
+      });
+    } else {
+      return passport.authenticate('local')(req, res, () => {
+        res.redirect('/tasklist');
+      });
     }
-    else
-    {
-      return passport.authenticate('local')(req,res,() =>{
-        res.redirect('/tasklist')
-      })
-    }
-  })
-})
-router.get('/logout',function(req,res,next){
-  req.logOut(function(err){
-    if(err)
-    {
+  });
+});
+
+router.get('/logout', function(req, res, next) {
+  req.logOut(function(err) {
+    if (err) {
       return next(err);
     }
-  })
-  res.redirect('/')
-})
+  });
+  res.redirect('/');
+});
+
 module.exports = router;
--- server/routes/task.js
+++ server/routes/task.js
@@ -1,57 +1,76 @@
+// routes/task.js
 var express = require('express');
 var router = express.Router();
-let Task = require('../model/task');
-let taskController = require('../controllers/task.js')
-function requireAuth(req,res,next)
-{
-    if(!req.isAuthenticated())
-    {
+let Task = require('../model/task');  // Assuming Task is a Mongoose model
+let taskController = require('../controllers/task.js');
+
+function requireAuth(req, res, next) {
+    if (!req.isAuthenticated()) {
         return res.redirect('/login');
     }
     next();
 }
 
+// Home Route - Pass TaskList to the home page
 router.get('/', async (req, res, next) => {
     try {
-        const TaskList = await Task.find();
+        const TaskList = await Task.find();  // Get tasks from the database
         res.render('index', {
             title: 'Home',
-            displayName: req.user?req.user.displayName:'',
-            TaskList: TaskList
+            displayName: req.user ? req.user.displayName : '',
+            TaskList: TaskList  // Pass TaskList to the view
         });
     } catch (err) {
         console.error(err);
         res.render('index', {
             error: 'Error on Server',
-            TaskList: []
+            TaskList: []  // Pass an empty array in case of error
         });
     }
 });
 
+// Home Route - Same logic as the '/' route
 router.get('/home', async (req, res, next) => {
     try {
-        const TaskList = await Task.find();
+        const TaskList = await Task.find();  // Get tasks from the database
         res.render('index', {
             title: 'Home',
-            displayName: req.user?req.user.displayName:'',
-            TaskList: TaskList
+            displayName: req.user ? req.user.displayName : '',
+            TaskList: TaskList  // Pass TaskList to the view
         });
     } catch (err) {
         console.error(err);
         res.render('index', {
             error: 'Error on Server',
-            TaskList: []
+            TaskList: []  // Pass an empty array in case of error
         });
     }
 });
 
+// Task List Route
 router.get('/tasklist', async (req, res, next) => {
     try {
-        const TaskList = await Task.find();
+        const TaskList = await Task.find();  // Get tasks from the database
         res.render('Task/list', {
             title: 'Tasks',
-            displayName: req.user?req.user.displayName:'',
-            TaskList: TaskList
+            displayName: req.user ? req.user.displayName : '',
+            TaskList: TaskList  // Pass TaskList to the view
+        });
+    } catch (err) {
+        console.error(err);
+        res.render('Task/list', {
+            error: 'Error on Server',
+            TaskList: []  // Pass an empty array in case of error
+        });
+    }
+});
+
+// Create Task - Display Add Task Form
+router.get('/add', async (req, res, next) => {
+    try {
+        res.render('Task/add', {
+            title: 'Add Task',
+            displayName: req.user ? req.user.displayName : ''
         });
     } catch (err) {
         console.error(err);
@@ -60,86 +79,72 @@ router.get('/tasklist', async (req, res, next) => {
         });
     }
 });
-/*Create Operation */
-router.get('/add',async(req,res,next)=>{
-    try{
-        res.render('Task/add',{
-            title: 'Add Task',
-            displayName: req.user?req.user.displayName:''
-        })
-    }
-    catch(err){
-        console.error(err)
-        res.render('Task/list',{
-            error:'Error on Server'})
-    }
-});
-/*Create Operation */
-router.post('/add',async(req,res,next)=>{
-    try{
-        let newTask = Task({
-            "Name":req.body.Name,
-            "Description":req.body.Description,
-            "Deadline":req.body.Deadline
-        })
-        Task.create(newTask).then(()=>{
-            res.redirect('/tasklist')
-        })
-    }
-    catch(err){
-        console.error(err)
-        res.render('Task/list',{
-            error:'Error on Server'})
+
+// Create Task - Handle Form Submission
+router.post('/add', async (req, res, next) => {
+    try {
+        let newTask = new Task({
+            "Name": req.body.Name,
+            "Description": req.body.Description,
+            "Deadline": req.body.Deadline
+        });
+        await Task.create(newTask);
+        res.redirect('/tasklist');
+    } catch (err) {
+        console.error(err);
+        res.render('Task/list', {
+            error: 'Error on Server'
+        });
     }
 });
-/*Update Operation */
-router.get('/edit/:id',async(req,res,next)=>{
-    try{
+
+// Edit Task - Display Edit Form
+router.get('/edit/:id', async (req, res, next) => {
+    try {
         const id = req.params.id;
-        const TaskToEdit=await Task.findById(id);
-        res.render('Task/edit',{
+        const TaskToEdit = await Task.findById(id);
+        res.render('Task/edit', {
             title: 'Edit Task',
-            displayName: req.user?req.user.displayName:'',
-            Task:TaskToEdit
-        })
-    }
-    catch{
+            displayName: req.user ? req.user.displayName : '',
+            Task: TaskToEdit
+        });
+    } catch (err) {
         console.error(err);
         next(err);
     }
 });
-/*Update Operation */
-router.post('/edit/:id',async(req,res,next)=>{
-    try{
-        let id= req.params.id;
-        let updatedTask = Task({
-            "_id":id,
-            "Name":req.body.Name,
-            "Description":req.body.Description,
-            "Deadline":req.body.Deadline
-        })
-        Task.findByIdAndUpdate(id,updatedTask).then(()=>{
-            res.redirect('/tasklist')
-        })
-    }
-    catch(err){
-        console.error(err)
-        res.render('Task/list',{
-            error:'Error on Server'})
+
+// Edit Task - Handle Form Submission
+router.post('/edit/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        let updatedTask = new Task({
+            "_id": id,
+            "Name": req.body.Name,
+            "Description": req.body.Description,
+            "Deadline": req.body.Deadline
+        });
+        await Task.findByIdAndUpdate(id, updatedTask);
+        res.redirect('/tasklist');
+    } catch (err) {
+        console.error(err);
+        res.render('Task/list', {
+            error: 'Error on Server'
+        });
     }
 });
-/*Delete Operation */
-router.get('/delete/:id',(req,res,next)=>{
-    try{
-        let id=req.params.id;
-        Task.deleteOne({_id:id}).then(()=>{
-            res.redirect('/tasklist')
-        })
-    }
-    catch{
-        console.error(err)
-        res.render('Task/list',{
-            error:'Error on Server'})
+
+// Delete Task
+router.get('/delete/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        await Task.deleteOne({ _id: id });
+        res.redirect('/tasklist');
+    } catch (err) {
+        console.error(err);
+        res.render('Task/list', {
+            error: 'Error on Server'
+        });
     }
 });
 module.exports = router;
